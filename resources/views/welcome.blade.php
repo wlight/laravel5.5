c987eab2e459b15c30eeedf420a52a7cc04e5475
wlight <404859395@qq.com>
2017-09-11 19:46:45 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 66px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Hello Laravel! - by Wlight</div>
+        </div>
+    </div>
+</body>
+</html>
