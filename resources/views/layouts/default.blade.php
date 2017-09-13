0f5e55656844948424ad9d870fd4a7ec4f9a8343
wlight <wormside@gmail.com>
2017-09-13 13:38:41 +0800

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <title>@yield('title', 'Sample') - Laravel5.5</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'staticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
