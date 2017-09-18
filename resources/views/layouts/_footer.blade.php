26662dd5ae068d9aa16d71577fd4a6ac4215c32e
wlight <wormside@gmail.com>
2017-09-18 16:59:01 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-1">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" alt="">
+            <a href="https://www.baidu.com/">
+                优帆远扬 | 创造不息，交付不止
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <a href="{{ route('about') }}">关于</a>
+            </ul>
+        </nav>
+    </footer>
+</div>
